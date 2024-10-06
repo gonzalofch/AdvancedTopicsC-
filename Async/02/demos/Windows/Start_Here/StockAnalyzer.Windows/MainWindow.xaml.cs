8396c0899f6017a9c4f26ce89d74a356ad9b63a0
gonzalofch <gmfloresch@gmail.com>
2024-10-06 17:26:53 +0200

    Add Explanation for Async Process with HttpClient

--- Async/02/demos/Windows/Start_Here/StockAnalyzer.Windows/MainWindow.xaml.cs
+++ Async/02/demos/Windows/Start_Here/StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 using StockAnalyzer.Core.Domain;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Windows;
 using System.Windows.Navigation;
@@ -20,30 +22,38 @@ public partial class MainWindow : Window
     }
 
 
-
-    private void Search_Click(object sender, RoutedEventArgs e)
+    private async void Search_Click(object sender, RoutedEventArgs e)
     {
         BeforeLoadingStockData();
-
-        var client = new WebClient();
-
-        var content = client.DownloadString($"{API_URL}/{StockIdentifier.Text}");
-
-        // Simulate that the web call takes a very long time
-        Thread.Sleep(10000);
-
-        var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
-
+        
+        //creates the httpClient to make the request
+        var client = new HttpClient();
+        
+        //uses the client / calls the api url in order to get a HttpResponse 
+        //StockIdentifier is the string passed by windows form input
+        //This Obtains a Task<HttpResponseMessage>
+        var responseTask = client.GetAsync($"{API_URL}/{StockIdentifier.Text}");
+        
+        // await , waits for return value of GetAsyncMethod (the HttpResponseMessage Itself)
+        var response = await responseTask;
+        
+        //here we are entering into the attributes of HttpResponseMessage, 
+        //In this case is content, has all the data in Json ready to be parsed to present in UI
+        var content = response.Content;
+        
+        //This awaits for the process of parse JSON data to String asynchronously
+        var contentData = await content.ReadAsStringAsync();
+        
+        //Use the Library Method JsonConvert to Deserialize in an IEnumerable of StockPrices objects
+        // returns this list of stockPrices 
+        var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(contentData);
+        
+        //Updates the grid and displays the data in the form
         Stocks.ItemsSource = data;
 
+        //esto solo añade mas informacion al final y cierra el proceso de carga de la barra verde
         AfterLoadingStockData();
     }
-
-
-
-
-
-
 
 
     private void BeforeLoadingStockData()
